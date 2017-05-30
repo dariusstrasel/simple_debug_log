0fcc96301e662a692e672f458b962527995e404e
Dstrasel <siruku@gmail.com>
2017-05-30 14:46:51 -0400

    Default setup.py package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='simple_debug_log',
+    version='0.0.1',
+    packages=['main'],
+    url='https://github.com/dariusstrasel/simple_debug_log',
+    license='MIT',
+    author='Dstrasel',
+    author_email='',
+    description='A Python library for creating priority-sorted debug to support application telemetry.'
+)
